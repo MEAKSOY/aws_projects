76bc3f3f098cb75cbc3410a318eff6800ba1ec56
MEAKSOY <mesataksoy@gmail.com>
2023-06-13 22:27:35 +0200

    Website config.

--- my-serverless-website/main.tf
+++ my-serverless-website/main.tf
@@ -14,29 +14,29 @@ resource "aws_s3_bucket" "website_bucket" {
 }
 
 resource "aws_s3_object" "website_bucket" {
-  bucket = aws_s3_bucket.website_bucket.id
-  key = "index.html"
-  source = "index.html"
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "index.html"
   content_type = "text/html"
 }
 
 resource "aws_s3_account_public_access_block" "website_bucket" {
-  block_public_acls = false
+  block_public_acls   = false
   block_public_policy = false
 }
 
 resource "aws_s3_bucket_public_access_block" "website_bucket" {
   bucket = aws_s3_bucket.website_bucket.id
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "website_bucket" {
   bucket = aws_s3_bucket.website_bucket.id
   policy = jsonencode({
-    Version = "2023-06-13"
+    Version = "2012-10-17"
     Statement = [
       {
         Sid = "PublicReadGetObject"
@@ -53,4 +53,11 @@ resource "aws_s3_bucket_policy" "website_bucket" {
       }
     ]
   })
+}
+
+resource "aws_s3_bucket_website_configuration" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
 }
