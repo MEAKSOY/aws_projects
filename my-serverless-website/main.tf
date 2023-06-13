2d315400a570b0b48fa50bc000ca4d564c935f84
MEAKSOY <mesataksoy@gmail.com>
2023-06-13 21:59:27 +0200

    Html object

--- my-serverless-website/main.tf
+++ my-serverless-website/main.tf
@@ -1,7 +1,21 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "us-east-1"
+    default_tags {
+      tags = {
+        project = "my-serverless-demo"
+        type = "web"
+        iac = "terraform"
+      }
+    }  
 }
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "esat-bucket-june13"
+}
+
+resource "aws_s3_object" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key = "index.html"
+  source = "index.html"
+  content_type = "text/html"
 }
