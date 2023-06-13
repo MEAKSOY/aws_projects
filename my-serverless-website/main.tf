ab39dc0955189f14589cb0ad4618ca67be76d30b
MEAKSOY <mesataksoy@gmail.com>
2023-06-13 22:09:03 +0200

    Bucket policy

--- my-serverless-website/main.tf
+++ my-serverless-website/main.tf
@@ -33,3 +33,24 @@ resource "aws_s3_bucket_public_access_block" "website_bucket" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsondecode({
+    Version = "2023-06-13"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+        ]
+        Resource = [
+          "${aws_s3_bucket.website_bucket.arn}",
+          "${aws_s3_bucket.website_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
