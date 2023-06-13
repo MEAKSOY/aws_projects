4bbbbec1b7ecb17e9a6e7e8f302b11059c66ed05
MEAKSOY <mesataksoy@gmail.com>
2023-06-13 22:03:41 +0200

    Bucket public access

--- my-serverless-website/main.tf
+++ my-serverless-website/main.tf
@@ -25,3 +25,11 @@ resource "aws_s3_account_public_access_block" "website_bucket" {
   block_public_policy = false
 }
 
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
